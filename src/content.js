f9e2d5d47891a6eb15e2058e74ede8c3997dfde7
Takahisa Ishikawa <takahisa_ishikawa@hammock.co.jp>
2024-08-19 21:18:00 +0900

    大量同時読み込みに成功

--- src/content.js
+++ src/content.js
@@ -1,46 +1,64 @@
-// 現在のページのコメント一覧ノード
-var currentCommentContainerNode = $('.comment' +  '.container');
+let currentCommentContainerNode;
 
-// ↓↓コメントノードを上から古い順に下に行くほど新しいコメントに並び変える。
-var commentIdList = [];
-var commentNodeList = [];
-currentCommentContainerNode.children('.comment-container').each(function () {
-  commentNodeList.push(this);
-  var id = Number(this.id);
-  if (isNaN(id)) {
-  } else {
-    commentIdList.push(id);
-  }
-});
-currentCommentContainerNode.empty();
-commentNodeList.reverse().forEach(x => currentCommentContainerNode.append(x));
-// ↑↑コメントノードを上から古い順に下に行くほど新しいコメントに並び変える。
-
-// ↓↓ひとつ前のページを読み込む。
-var min = Number.MAX_SAFE_INTEGER;
-for (var j = 0; j < commentIdList.length; j++) {
-  if (min > commentIdList[j]) {
-    min = commentIdList[j];
-  }
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-var currentPageUrl = new URL(window.location);
-var lastIndex = currentPageUrl.pathname.lastIndexOf('/', -2 + currentPageUrl.pathname.length);
-var userName = currentPageUrl.pathname.substring(1 + lastIndex);
-userName = userName.substring(0, -1 + userName.length);
+async function loadCommentPage(address) {
+  await fetch(address) //【URL】
+    .then(response => response.text()) .then(data => { 
+      const html = new DOMParser().parseFromString(data, "text/html");
+      const previousCommentContainerNodeList = html.querySelectorAll('div.comment.container div.comment-container');
+      for (var k = 0; k < previousCommentContainerNodeList.length; k ++) {
+        var newNode = document.createElement("div");
+        newNode.innerHTML = previousCommentContainerNodeList[k].outerHTML;
+        currentCommentContainerNode.prepend(newNode.firstChild);
+      }
+  });
+}
 
-var previousPageAddress = "https://suki-kira.com/people/result/" + userName + "/?nxc=" + min;
-fetch(previousPageAddress) //【URL】
-  .then(response => response.text()) .then(data => { 
-    const html = new DOMParser().parseFromString(data, "text/html");
-    const previousCommentContainerNodeList = html.querySelectorAll('div.comment.container div.comment-container');
-    for (var k = 0; k < previousCommentContainerNodeList.length; k ++) {
-      var newNode = document.createElement("div");
-      newNode.innerHTML = previousCommentContainerNodeList[k].outerHTML;
-      currentCommentContainerNode.prepend(newNode.firstChild);
+async function MyExtension() {
+  // 現在のページのコメント一覧ノード
+  currentCommentContainerNode = $('.comment' +  '.container');
+
+  // ↓↓コメントノードを上から古い順に下に行くほど新しいコメントに並び変える。
+  var commentNodeList = [];
+  currentCommentContainerNode.children('.comment-container').each(function () {
+    commentNodeList.push(this);
+  });
+  currentCommentContainerNode.empty();
+  commentNodeList.reverse().forEach(x => currentCommentContainerNode.append(x));
+  // ↑↑コメントノードを上から古い順に下に行くほど新しいコメントに並び変える。
+
+  // ↓↓100個前までのページを読み込む。
+  for (var i = 0; i < 20; i ++) {
+    var min = currentCommentContainerNode.first().get(0).firstChild.id;
+    var currentPageUrl = new URL(window.location);
+    var lastIndex = currentPageUrl.pathname.lastIndexOf('/', -2 + currentPageUrl.pathname.length);
+    var userName = currentPageUrl.pathname.substring(1 + lastIndex);
+    userName = userName.substring(0, -1 + userName.length);
+    var previousPageAddress = "https://suki-kira.com/people/result/" + userName + "/?nxc=" + min;
+    await loadCommentPage(previousPageAddress);
+
+    // 「前へ」のリンク先を変更する。
+    min = currentCommentContainerNode.first().get(0).firstChild.id;
+    var nextPageAddress = "/people/result/" + userName + "/?nxc=" + min;    
+    var aNodeList = document.getElementsByTagName('a');
+    for (const aNode of aNodeList) {
+      if (aNode.href.includes("people/result/" + userName + "/?nxc=")) {
+        aNode.href = nextPageAddress;
+      }
     }
-});
-// ↑↑ひとつ前のページを読み込む。
+    
+    // サーバー負荷を避けるためスリープを入れておく。
+    await sleep(3000);
+  }
+  // ↑↑100個前までのページを読み込む。
+}
+
+MyExtension();
+
+
 
 
 // window.alert(min);
